04dd6a58b2928855745c7d6d3cc4a36925135a5b
Zadnessa <mois.pave@gmail.com>
2025-06-16 01:34:42 +0300

    one more fix

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -15,7 +15,7 @@ class AppRoutes {
   static const String expenseHistoryScreen = '/expense-history-screen';
   static const String analyticsDashboardScreen = '/analytics-dashboard-screen';
   static const String settingsScreen = '/settings-screen';
-}
+
   static Map<String, WidgetBuilder> routes = {
     initial: (context) => const MainScreen(),
     mainScreen: (context) => const MainScreen(),
@@ -24,4 +24,5 @@ class AppRoutes {
     expenseHistoryScreen: (context) => const ExpenseHistoryScreen(),
     analyticsDashboardScreen: (context) => const AnalyticsDashboardScreen(),
     settingsScreen: (context) => const SettingsScreen(),
-};
+  };
+}
