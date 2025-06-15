f9445789cb5b8b5b158406f9a53a74b2054f55df
Zadnessa <mois.pave@gmail.com>
2025-06-16 01:14:43 +0300

    Update routes for main screen navigation

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -5,10 +5,11 @@ import '../presentation/category_selection_screen/category_selection_screen.dart
 import '../presentation/expense_history_screen/expense_history_screen.dart';
 import '../presentation/settings_screen/settings_screen.dart';
 import '../presentation/analytics_dashboard_screen/analytics_dashboard_screen.dart';
+import '../presentation/main_screen/main_screen.dart';
 
 class AppRoutes {
-  static const String initial =
-      '/expense-entry-screen'; // Изменено на прямой переход к экрану ввода расходов
+  static const String initial = '/';
+  static const String mainScreen = '/';
   static const String expenseEntryScreen = '/expense-entry-screen';
   static const String categorySelectionScreen = '/category-selection-screen';
   static const String expenseHistoryScreen = '/expense-history-screen';
@@ -16,11 +17,11 @@ class AppRoutes {
   static const String settingsScreen = '/settings-screen';
 
   static Map<String, WidgetBuilder> routes = {
-    initial: (context) => const ExpenseEntryScreen(),
+    initial: (context) => const MainScreen(),
+    mainScreen: (context) => const MainScreen(),
     expenseEntryScreen: (context) => const ExpenseEntryScreen(),
     categorySelectionScreen: (context) => const CategorySelectionScreen(),
     expenseHistoryScreen: (context) => const ExpenseHistoryScreen(),
     analyticsDashboardScreen: (context) => const AnalyticsDashboardScreen(),
     settingsScreen: (context) => const SettingsScreen(),
-  };
-}
+};
